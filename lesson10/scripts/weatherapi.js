c91a25e3b56f937dd2122f89b48703e0aab48e55
Glen Reynolds <=>
2020-03-12 10:33:52 -0600

    lesson 10

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -9,6 +9,25 @@ response => {
 .then(
     (jsObject) => {
         console.log(jsObject);
+
+        let t = jsObject.main.temp;
+        let s = jsObject.wind.speed;
+        let f = 0;
+        
+      
+        
+        if (t <= 50 && s >= 3) {
+            f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+            document.querySelector('#wind-chill').innerHTML = f.toFixed(0) + '&deg;';
+        } else {
+            f = 'N/A';
+            document.querySelector('#wind-chill').innerHTML = f;
+        }
+        
+        document.querySelector('#current-temp').innerHTML = `${t}&deg;`;
+        document.querySelector('#wind-speed').innerHTML = s + 'mph';
+        
+    
     
         document.querySelector('#current-temp')
     .textContent = jsObject.main.temp;
