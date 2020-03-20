d42b907f45d43785036c0c24bd05e7972807544e
Glen Reynolds <=>
2020-03-19 21:37:45 -0600

    finished

---
+++ lesson11/scripts/sample.js
@@ -0,0 +1,65 @@
+document.querySelector('#current-temp').innerHTML = '50&deg;';
+document.querySelector('#current-temp').innerHTML = '10mph;';
+document.querySelector( '#humidity').innerHTML = '10&percnt';
+
+const currentDate = new Date();
+const aside = document.querySelector('aside');
+if (currentDate.getDate()=== 5) {
+    //aside.style.display = 'block';//
+    aside.classList.remove('hidden');
+} else {
+    //aside.style.display ='none';
+    //aside.classList.add('hidden');
+}
+
+if (currentDate.getDay()  !==5  ) {
+    aside.classList.add('hidden');
+}
+
+let f, t, s;
+t = 50;
+s = 50;
+
+f = 35.74 + 0.6215 * t -35.75 * Math.pow(s, 0.16) + 0.4275 * t  * Math.pow(s, 16);
+document.querySelector('WindChill').innerHTML = f.toFixed(2);
+if (t <= 50 && s >= 3) {
+   f =35.74 + 0.6215 * t -35.75 * Math.pow(s, 0.16) + 0.4275 * t  * Math.pow(s, 16);
+} else {
+ f ='N/A';
+}
+
+
+document.querySelector('WindChill').innerHTML = f;
+
+
+document.querySelector('currentTemp').innerHTML ='${t}&deg';
+document.querySelector('#wind-speed').innerHTML = s + 'mph';
+
+const currentDate = new Date();
+const aside = document.querySelector('aside');
+if(currentDate.getDay() === 4) {
+    aside.classList.remove('hidden');
+}
+
+WebFont.load({
+    google: {
+      families: [
+         'Siracha'
+      ]
+    }
+  });
+    
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('#current-temp').innerHTML = t + '&deg;' // '50&deg;';
+document.querySelector('#current-temp').innerHTML =  s + 'mph';//'10mph;'
+document.querySelector( '#humidity').innerHTML =  f + '&deg';//'10&percnt';
---
+++ lesson11/scripts/windchill.js
@@ -0,0 +1,27 @@
+let f, t, s;
+t = 50;
+s = 10;
+
+if (t <= 50 && s >= 3) {
+    f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    document.querySelector('#wind-chill').innerHTML = f.toFixed(0) + '&deg;';
+} else {
+    f = 'N/A';
+    document.querySelector('#wind-chill').innerHTML = f;
+}
+
+document.querySelector('#current-temp').innerHTML = `${t}&deg;`;
+document.querySelector('#wind-speed').innerHTML = s + 'mph';
+
+document.querySelector('#weatherDesc').textContent =
+jsObject.weather[0].main;
+
+letimageUrl =`https://openweather.org/img/w/${jsObject.weather[0].icon}.png`;
+
+document.querySelector('weatherImage')
+.setAttribute('src', imageUrl);
+
+document.querySelector('weatherImage')
+.setAttribute('alt', jsObject.weather[0].main);
+ 
+
